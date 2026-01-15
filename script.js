4cc201f95001c899ab35b2f6d3eb4c0a24ec8edb
test <koush0811@gmail.com>
2026-01-15 16:51:00 +0900

    作品紹介追加

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ const main = document.querySelector('main')
 
 const swiper = new Swiper(".swiper",{
     loop: true,
-    autoplay: {
-        delay: 3000,
-        disableOnInteraction: false
+    
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     },
 })
 
